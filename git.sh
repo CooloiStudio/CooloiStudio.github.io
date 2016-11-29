7533987334d5b1b7a799e0cd55e7375b92404128
Hytran <shaokang@shoplex.com>
2016-11-29 12:08:25 +0800

    update

--- git.sh
+++ git.sh
@@ -3,7 +3,7 @@
 datetime=`date +%Y%m%d%H%M`
 
 git config user.name = "浮世絵空事"
-git config user.email = "UkiyoESoragoto"
+git config user.email = "UkiyoESoragoto@gmail.com"
 git add --a
 git commit -m "$datetime"
 git push
